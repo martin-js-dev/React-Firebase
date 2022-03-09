4bd9f3397e7b32adb671f77f2a2eb0e562cec0b4
Martin Velinov <esim_player@hotmail.com>
2022-03-09 21:34:16 +0100

    new change

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBYE3oZBLDDDPgj8v6m_j65K6RFJZmz5n8",
+    authDomain: "react-framer-motion.firebaseapp.com",
+    projectId: "react-framer-motion",
+    storageBucket: "react-framer-motion.appspot.com",
+    messagingSenderId: "745245836202",
+    appId: "1:745245836202:web:5ca7d73dc1c35be72ea1d3",
+    measurementId: "G-Y8WSVTT94W"
+  };
+
+  firebaseConfig.initializeApp(firebaseConfig);
+
+ 
