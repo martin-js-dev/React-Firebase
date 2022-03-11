a474cf6a44b616d1c5ee8b63134ab3bccd9ea820
Martin Velinov <esim_player@hotmail.com>
2022-03-11 22:34:05 +0100

    new change

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Hero = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
+      <h1 className="text-3xl font-bold underline">FireGram</h1>
       <h2>Your Pictures</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
